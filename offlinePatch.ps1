031c1bb5a19cd43d0702394e2ad0c5cc50455bac
Brendan Sturges <brendansturges@gmail.com>
2016-05-23 14:55:03 -0400

    removing unneccessary functionality (choose folder) & added better log file names

--- offlinePatch.ps1
+++ offlinePatch.ps1
@@ -1,42 +1,16 @@
-Function Get-Folder($initialDirectory) {
-    [Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
-    [System.Windows.Forms.Application]::EnableVisualStyles()
-    $browse = New-Object System.Windows.Forms.FolderBrowserDialog
-    $browse.RootFolder = [System.Environment+SpecialFolder]'MyComputer'
-    $browse.ShowNewFolderButton = $false
-    $browse.Description = "Choose patch source directory"
+$location = (get-item -path ".\" -verbose).fullname
 
-    $loop = $true
-    while($loop)
-    {
-        if ($browse.ShowDialog() -eq "OK")
-        {
-            $loop = $false
-        } else
-        {
-            $res = [System.Windows.Forms.MessageBox]::Show("You clicked Cancel. Try again or exit script?", "Choose a directory", [System.Windows.Forms.MessageBoxButtons]::RetryCancel)
-            if($res -eq "Cancel")
-            {
-                return
-            }
-        }
-    }
-    $browse.SelectedPath
-    $browse.Dispose()
-}
-
-$folderLoc = Get-Folder
-$logLocation = (get-item -path ".\" -verbose).fullname
-
-$patches = (Get-ChildItem -path $folderLoc -filter "*.msu").name
+$patches = (Get-ChildItem -path $location -filter "*.msu").name
 
 $i = 0
+$date = (Get-Date).toString('MM-dd-yyyy')
+$systemName = $env:computername
 
 foreach($patch in $patches){
 	$i++
 	Write-Progress -id 1 -activity "Attempting to patch: $patch `($i of $($patches.count)`)" -percentComplete ($i / ($patches.Count)*100)
 	Try{
-		$argList = "$patch /install /quiet /norestart /log:$patch.evtx"
+		$argList = "$patch /install /quiet /norestart /log:$($date)_$($systemName)_$($patch).evtx"
 		Start-Process -filepath 'C:\windows\system32\wusa.exe' -argumentList $argList -noNewWindow -Wait
 		Write-Host "$patch applied"
 	}
