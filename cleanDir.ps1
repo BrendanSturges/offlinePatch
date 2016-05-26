78836adedfcdb1533ac3fec1d8b3df293912b47e
Brendan Sturges <BrendanSturges@users.noreply.github.com>
2016-05-26 09:32:06 -0400

    added .exe support

--- cleanDir.ps1
+++ cleanDir.ps1
@@ -1,1 +1,2 @@
-Get-ChildItem -recurse -filter "*.msu" | % {Copy-Item -path $_.FullName -Destination (get-item -path ".\" -verbose).fullname}
+$includeFiles = ("*.msu","*.exe")
+Get-ChildItem -recurse -include $includeFiles | % {Copy-Item -path $_.FullName -Destination (get-item -path ".\" -verbose).fullname}
